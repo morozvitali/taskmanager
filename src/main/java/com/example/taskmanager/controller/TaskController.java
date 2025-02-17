85cccd2b5f8ee2d53b1f3733029d7accebc79c68
morozvitali <morozvitali99@gmail.com>
2025-02-17 10:11:55 +0200

    Add a method that will handle a GET request to display the form and a POST request to process the form data

--- src/main/java/com/example/taskmanager/controller/TaskController.java
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -22,11 +23,18 @@ public class TaskController {
     }
 
     @GetMapping("/tasks")
-    public String getTasks(@RequestParam(name = "newTask", required = false) String newTask, Model model) {
-        if (newTask != null && !newTask.trim().isEmpty()) {
-            tasks.add(newTask);
-        }
+    public String getTasks(Model model) {
         model.addAttribute("tasks", tasks);
         return "tasks";
+    }
+
+
+    @PostMapping("/tasks")
+    public String addTasks(@RequestParam("taskName") String taskName) {
+        if (taskName != null && !taskName.trim().isEmpty()) {
+            tasks.add(taskName);
+        }
+
+        return "redirect:/tasks"; // перенаправлення назад до списку
     }
 }
